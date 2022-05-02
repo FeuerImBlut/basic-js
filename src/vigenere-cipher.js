426a1d3bc22f2ce264c131d436ae5f25dc1d5e52
Alexander Golubovich <golubovich.as@gmail.com>
2022-05-02 19:56:00 +0300

    feat: implement vigenere

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -10,7 +10,7 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  * const reverseMachine = new VigenereCipheringMachine(false);
  * 
- * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
+ * directMachine.encrypt('attack at dawn!', 'a  phonse') => 'AEIHQX SX DLLU!'
  * 
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
  * 
@@ -20,13 +20,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(type = true) {
+    this.type = type;
+    const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+    const alphabet = alpha.map((x) => String.fromCharCode(x));
+    let alphabetCopy = [...alphabet];
+    let tabulaRecta = [];
+    tabulaRecta.push(alphabet);
+    for (let i = 1; i < alphabet.length; i++) {
+      let temp = alphabetCopy.shift();
+      alphabetCopy.push(temp);
+      let pushedCopy = [...alphabetCopy];
+      tabulaRecta.push(pushedCopy);
+    }
+    this.tabulaRecta = tabulaRecta;
+    this.alphabet = alphabet;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(str, key) {
+    if (str === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+
+    let result = [];
+    let keyLength = key.length;
+    for (let i = 0, k = 0; i < str.length; i++) {
+      if (!this.alphabet.includes(str[i].toUpperCase())) {
+        result.push(str[i]);
+        continue;
+      }
+      let keyIndex = this.alphabet.indexOf(key[k].toUpperCase());
+      let letterIndex = this.alphabet.indexOf(str[i].toUpperCase());
+      result.push(this.tabulaRecta[letterIndex][keyIndex]);
+      k++;
+      k = (k == keyLength) ? 0 : k;
+    }
+    if (!this.type) return result.reverse().join('');
+    return result.join('');
+  }
+  decrypt(str, key) {
+    if (str === undefined || key === undefined) {
+      throw new Error('Incorrect arguments!');
+    }
+    return;
+    // throw new NotImplementedError('Not implemented');
+    // // remove line with error and write your code here
   }
 }
 
